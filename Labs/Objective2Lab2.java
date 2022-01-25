d96a41b3c1a75ba5247796cabc05bb2efbf4dbf7
Brent Grauerholz <brentgrauerholz@yahoo.com>
2022-01-25 13:07:35 -0600

    Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Brent Grauerholz";
+    int age = 43;
+    String state = "Wisconsin";
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(state);
+  }
+}
