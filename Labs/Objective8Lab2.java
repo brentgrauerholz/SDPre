5b7918a29c6904fe7bfbd9bc83b45849a6b9380c
Brent Grauerholz <brentgrauerholz@yahoo.com>
2022-02-03 18:01:58 -0600

    complete Objective8Lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,11 @@
+public class Objective8Lab2 {
+  public static void main(String[] args) {
+    int num = 20;
+    int sum = 0;
+
+    for(int i = 1; i <= num; ++i) {
+      sum += i;
+    }
+    System.out.println(sum);
+    }
+  }
